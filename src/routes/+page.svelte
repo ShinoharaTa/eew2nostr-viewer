484b3617996091cc76502105dbc4bc4123bc4e01
tshinohara <takuma1201@gmail.com>
2024-06-12 21:03:48 +0900

    テストモード用のチェックボックスを追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { writable } from 'svelte/store';
   import { NostrApp, UniqueEventList, Nostr } from "nosvelte";
   import { createRxForwardReq } from "rx-nostr";
   import "websocket-polyfill";
@@ -8,13 +9,25 @@
 
   const req = createRxForwardReq();
   const relays: string[] = ["wss://relay-jp.shino3.net", "wss://r.kojira.io"];
+  const isTestMode = writable(true);
   const sorted = (events: Nostr.Event[]) => {
     return [...events].sort((a, b) => b.created_at - a.created_at);
+  };
+  const testMode = (events: Nostr.Event[]) => {
+    if($isTestMode) return events
+    return events.filter((item) =>
+      item.tags.some((tagArray) => tagArray.includes("eew_alert_system_by_shino3")),
+    );
   };
   const first = (events: Nostr.Event[]) => {
     return events[0];
   };
 </script>
+
+<div class="top-right-checkbox form-check">
+  <input class="form-check-input" type="checkbox" id="testModeCheckbox" bind:checked={$isTestMode}>
+  <label class="form-check-label" for="testModeCheckbox">テストモード</label>
+</div>
 
 <NostrApp {relays}>
   <UniqueEventList
@@ -41,8 +54,7 @@
     </div>
 
     <div class="page">
-      <EEW content={first(sorted(events)).content}></EEW>
-      <!-- <button class="btn btn-secondary info-button"> ？ </button> -->
+      <EEW content={first(testMode(sorted(events))).content}></EEW>
     </div>
   </UniqueEventList>
 </NostrApp>
